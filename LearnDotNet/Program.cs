a81087b2cfb09714c3ac3b8793edf3ac35165fb4
X Coan <xjcoan@gmail.com>
2017-11-11 16:09:36 -0600

    Initial commit

---
+++ LearnDotNet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LearnDotNet
+{
+    class TestClass 
+    {
+        // User input String
+        String myString;
+
+        // Constructor
+        public TestClass(string inputString) 
+        {
+            myString = inputString;
+        }
+
+        public void printString()
+        {
+            Console.WriteLine("You entered: ");
+            Console.WriteLine(myString);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input String: ");
+            String userInput = Console.ReadLine();
+            TestClass testClass = new TestClass(userInput);
+            testClass.printString();
+        }
+    }
+}
